e650ef7833025954f9da638119ab22437f1426ff
André M Piva <andrempiva@gmail.com>
2026-05-22 17:44:59 -0300

    moved files to src folder

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
 int main(int argc, char *argv[]) {
     if (argc == 1) {
         printf("Usage: %s [-o <rom_file>] [-t <rom_file_number>]\n", argv[0]);
-        return 1;
+        return 0;
     }
 
     int action_result = init(argc, argv);
